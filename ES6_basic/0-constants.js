28a0dbd5bb5ae113e56f0e340bc348b6cbdd2a36
NikkiMerena <alderman.nikki1022@gmail.com>
2023-05-11 11:25:07 -0500

    fixed error

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,16 +1,15 @@
-/* eslint-disable no-unused-vars */
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
-  }
-  
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getLast();
-  
-    return combination;
-  }
+  var task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  var combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
