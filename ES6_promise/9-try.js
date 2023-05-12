37023022f5a3d674f7ad68338646f25de17666d3
NikkiMerena <alderman.nikki1022@gmail.com>
2023-05-11 23:32:24 -0500

    task9

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,23 +1,23 @@
 export default function guardrail(mathFunction) {
-    // Function to handle a math function with error handling
+  // Function to handle a math function with error handling
 
-    const queue = [];
-    // Create an empty array to store the queue of items
+  const queue = [];
+  // Create an empty array to store the queue of items
 
-    try {
-      queue.push(mathFunction());
-      // Try executing the mathFunction and push the result into the queue array
-    } catch (e) {
-      queue.push('Error: '.concat(e.message));
-      // If an error occurs during the execution of mathFunction, push an
-      // error message into the queue array
-    } finally {
-      queue.push('Guardrail was processed');
-      // Regardless of whether an error occurred or not, push a message into the queue
-      // array indicating that the guardrail was processed
-    }
+  try {
+    queue.push(mathFunction());
+    // Try executing the mathFunction and push the result into the queue array
+  } catch (e) {
+    queue.push('Error: '.concat(e.message));
+    // If an error occurs during the execution of mathFunction, push an
+    // error message into the queue array
+  } finally {
+    queue.push('Guardrail was processed');
+    // Regardless of whether an error occurred or not, push a message into the queue
+    // array indicating that the guardrail was processed
+  }
 
-    return queue;
-    // Return the queue array containing the result of mathFunction (if successful)
-    // and the guardrail messages
+  return queue;
+  // Return the queue array containing the result of mathFunction (if successful)
+  // and the guardrail messages
 }
