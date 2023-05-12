76694d9a27ccaaf3b39f94c7319d0f2a19954bf7
NikkiMerena <alderman.nikki1022@gmail.com>
2023-05-11 23:21:57 -0500

    task8

---
+++ ES6_promise/8-try.js
@@ -0,0 +1,13 @@
+export default function divideFunction(numerator, denominator) {
+  // Function to divide two numbers
+
+  if (denominator === 0) {
+    // Check if the denominator is equal to 0
+    // If it is, throw an Error with the message 'cannot divide by 0'
+    throw new Error('cannot divide by 0');
+  }
+
+    return (numerator / denominator);
+    // Return the result of dividing the numerator by the denominator
+}
+  
