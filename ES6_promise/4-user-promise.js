0f40038da622a5bc126b175bf73010e767a347be
NikkiMerena <alderman.nikki1022@gmail.com>
2023-05-11 19:26:21 -0500

    task4

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -3,5 +3,4 @@ export default function signUpUser(firstName, lastName) {
 
   return Promise.resolve({ firstName, lastName });
   // Creates Promise that resolves immed. w/ an object containing the provided firstName & lastName
-}
-// End of signUpUser function  
+}  
