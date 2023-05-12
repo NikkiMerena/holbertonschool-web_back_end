d140ed804e80ddbf7fa8598f3aba0f6a99437a3a
NikkiMerena <alderman.nikki1022@gmail.com>
2023-05-11 21:19:45 -0500

    task7

--- ES6_promise/7-load_balancer.js
+++ ES6_promise/7-load_balancer.js
@@ -1,9 +1,8 @@
 export default function loadBalancer(chinaDownload, USDownload) {
   // Function to perform load balancing between China and US downloads
-  
+
   return Promise.race([chinaDownload, USDownload]);
   // Returns a new Promise that resolves or rejects as soon as one of the promises
   // in the iterable resolves or rejects
   // The promises being compared are 'chinaDownload' and 'USDownload'
-}
-  
+}  
