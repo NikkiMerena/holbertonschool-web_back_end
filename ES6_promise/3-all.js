07b619b22664bcb91a44b4525afeed63d169afba
NikkiMerena <alderman.nikki1022@gmail.com>
2023-05-11 19:11:44 -0500

    task3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -11,16 +11,16 @@ export default function handleProfileSignup() {
     .then((values) => {
     // Attach a callback function to be executed when the Promise.all resolves successfully.
     // The resolved values of the promises are passed as an array to the callback function.
-  
+
       console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
       // Log a message to the console using the body of the first resolved promise value
       // and the firstName and lastName properties of the second resolved promise value.
     })
-   
+
     .catch(() => {
     // Attach a callback function to be executed when an error occurs in any of the promises.
     // The callback function logs the message 'Signup system offline' to the console.
-      
+
       console.log('Signup system offline');
     });
 }
