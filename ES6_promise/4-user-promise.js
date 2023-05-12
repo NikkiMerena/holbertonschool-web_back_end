40c10b9f995891ca298c163d3e56a7a0dceeca77
NikkiMerena <alderman.nikki1022@gmail.com>
2023-05-11 19:20:00 -0500

    task4

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,9 +1,8 @@
 export default function signUpUser(firstName, lastName) {
-    // Function to sign up a user
+  // Function to sign up a user
 
   return Promise.resolve({ firstName, lastName });
-  // Creates a Promise that resolves immediately with an object containing the provided firstName and lastName
+  // Creates a Promise that resolves immediately w/ an object containing the provided firstName & lastName
 }
- 
-// End of signUpUser function
-  
+
+// End of signUpUser function  
