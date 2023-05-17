22b77db8834c8c3722ebf6d14c53eeff05b9b24e
NikkiMerena <alderman.nikki1022@gmail.com>
2023-05-17 11:42:04 -0500

    task1

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,11 +1,9 @@
 import ClassRoom from './0-classroom';
 
-function initializeRooms() {
-  const roomSizes = [19, 20, 34];
-  const rooms = roomSizes.map((size) => new ClassRoom(size));
-  return rooms;
-}
+export default function initializeRooms() {
+  const room1 = new ClassRoom(19);
+  const room2 = new ClassRoom(20);
+  const room3 = new ClassRoom(34);
 
-// Testing the initializeRooms function
-const rooms = initializeRooms();
-console.log(rooms);
+  return [room1, room2, room3];
+}
