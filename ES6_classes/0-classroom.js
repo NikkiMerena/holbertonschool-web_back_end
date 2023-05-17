84d252896448c3cebad9d334f5cd0ab56f3653f9
NikkiMerena <alderman.nikki1022@gmail.com>
2023-05-17 11:26:49 -0500

    adding files

---
+++ ES6_classes/0-classroom.js
@@ -0,0 +1,8 @@
+export default class ClassRoom {
+  // define a class named ClassRoom & export it as the default export
+  constructor(maxStudentsSize) {
+    // Define a constructor function that takes a parameter named maxStudentsSize
+    this._maxStudentsSize = maxStudentsSize;
+    // Assign the value of the maxStudentsSize parameter to the instance variable _maxStudentsSize
+  }
+}
