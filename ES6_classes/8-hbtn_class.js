df1038dc7599a5f719ab5364241594f9e9e7d7d8
NikkiMerena <alderman.nikki1022@gmail.com>
2023-05-17 22:31:42 -0500

    task8

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,24 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](type) {
+    if (type === 'number') {
+      return (this._size);
+    }
+    if (type === 'string') {
+      return (this._location);
+    }
+    return null;
+  }
+
+  // valueOf() {
+  //   return (this._size);
+  // }
+
+  // toString() {
+  //   return (this._location);
+  // }
+}
