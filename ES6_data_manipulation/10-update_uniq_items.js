6d11f113a2d259b733923c14606f7bd47f0e6b10
NikkiMerena <alderman.nikki1022@gmail.com>
2023-05-20 20:27:28 -0500

    task10

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,19 @@
+function updateUniqueItems(groceryMap) {
+  if (!(groceryMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  const updatedMap = new Map();
+
+  for (const [item, quantity] of groceryMap) {
+    if (quantity === 1) {
+      updatedMap.set(item, 100);
+    } else {
+      updatedMap.set(item, quantity);
+    }
+  }
+
+  return updatedMap;
+}
+
+export default updateUniqueItems;
