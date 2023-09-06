dcb96028b9376faf18cc4bf638a2742939307562
NikkiMerena <alderman.nikki1022@gmail.com>
2023-09-06 10:31:19 -0500

    trying again

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """type-annotated function takes two floats returns the sum as a float"""
 
-def add(a: float, b: float) -> float:
 
+def add(a: float, b: float) -> float:
 
     """Returns the sum of two floats"""
     return a + b
