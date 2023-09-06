15b98b905e718557eb89a54e2a2a275b1ebb515d
NikkiMerena <alderman.nikki1022@gmail.com>
2023-09-06 10:43:25 -0500

    task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module to provide a function to compute the floor of a float."""
+
+import math
+
+def floor(n: float) -> int:
+    """Return the floor of a float.
+
+    Args:
+        n (float): The floating point number.
+
+    Returns:
+        int: The floor of the float.
+    """
+    return math.floor(n)
