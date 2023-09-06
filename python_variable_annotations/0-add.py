2ad4dee424e507b67d3672f414f637c2d4b70468
NikkiMerena <alderman.nikki1022@gmail.com>
2023-09-06 10:22:48 -0500

    trying to fix pycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""type-annotated function takes two floats returns the sum as a float"""
 
 def add(a: float, b: float) -> float:
+    """Returns the sum of two floats"""
     return a + b
