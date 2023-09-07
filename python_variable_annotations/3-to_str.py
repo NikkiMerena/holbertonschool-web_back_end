a85d123dba0e1fbd478a5f83d7cd6cd5ed655eb1
NikkiMerena <alderman.nikki1022@gmail.com>
2023-09-07 10:18:47 -0500

    task3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""This module contains a function that converts a float to its string rep."""
+
+
+def to_str(n: float) -> str:
+    """Takes a float n as argument and returns the string rep of the float.
+
+    Args:
+        n (float): the float to convert to a string.
+
+    Returns:
+        str: the string representation of the float.
+    """
+    return str(n)
