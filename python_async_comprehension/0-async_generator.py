8535f82d5a87fff4409ec51a5711e8d861c07ae7
NikkiMerena <alderman.nikki1022@gmail.com>
2023-09-07 22:11:33 -0500

    fixed error

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """Module for storing the async_generator coroutine."""
 
-from typing import AsyncGenerator
+from typing import Generator
 import asyncio
 import random
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Coroutine to loop 10 times, wait 1 sec, yield
     random number between 0 and 10."""
     for _ in range(10):
