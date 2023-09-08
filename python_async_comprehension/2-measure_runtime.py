9e6b3385ab404e7adb1a9e35d0fd987510987913
NikkiMerena <alderman.nikki1022@gmail.com>
2023-09-07 22:35:49 -0500

    meant to add last task only but add all again

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -4,12 +4,12 @@
 import asyncio
 from time import time
 from typing import List
-async_comprehensions = __import__('1-async_comprehension').async_comprehension
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """Coroutine to execute async_comprehension four times in parallel using
-    asyncio.gather and measure the total runtime."""
+    """Coroutine to execute async_comprehension four times in parallel
+    using asyncio.gather and measure the total runtime."""
     start_time = time()
 
     await asyncio.gather(
