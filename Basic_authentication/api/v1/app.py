d127f308988f756565e05bf5e8362065505aab28
NikkiMerena <alderman.nikki1022@gmail.com>
2023-09-25 14:01:00 -0500

    forgot to push app.py should fix error

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -2,16 +2,46 @@
 """
 Route module for the API
 """
+import os
 from os import getenv
-from api.v1.views import app_views
+
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
+
+from api.v1.views import app_views
+from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+auth = None
+if os.getenv("AUTH_TYPE") == "auth":
+    auth = Auth()
+
+
+@app.before_request
+def before_request_func():
+    """Handling before_request."""
+    if auth is None:
+        return
+
+    # List of paths that don't need authentication
+    exempt_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                    '/api/v1/forbidden/']
+
+    # Check if request.path is not in the exempt_paths
+    if not auth.require_auth(request.path, exempt_paths):
+        return
+
+    # If auth.authorization_header(request) returns None, raise the error 401
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    # If auth.current_user(request) returns None, raise the error 403
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 @app.errorhandler(404)
@@ -39,4 +69,4 @@ if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
-    app.run(debug=true)
+    app.run(debug=True)
