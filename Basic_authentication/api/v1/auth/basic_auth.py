aa015cf4c96a4d874c6aa4fe90f2364347fd9e5a
NikkiMerena <alderman.nikki1022@gmail.com>
2023-09-25 14:21:57 -0500

    forgot to doc

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
-"""basic auth"""
+"""Module for BasicAuth class"""
+
 from .auth import Auth
 
 class BasicAuth(Auth):
+    """BasicAuth class
+    
+    This class is used for handling Basic Authentication
+    It inherits from the Auth class
+    """
     pass
