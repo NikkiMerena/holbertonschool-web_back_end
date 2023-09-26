fcbca5537288d9e09c359a6996ad541ade51aab0
NikkiMerena <alderman.nikki1022@gmail.com>
2023-09-25 22:42:28 -0500

    task8

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """Module for BasicAuth class"""
 
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -10,6 +11,31 @@ class BasicAuth(Auth):
     This class is used for handling Basic Authentication
     It inherits from the Auth class
     """
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Returns the decoded value of a Base64 string
+            base64_authorization_header.
+
+        Args:
+            base64_authorization_header (str): The Base64 encoded string.
+
+        Returns:
+            str: The decoded value as a UTF8 string if
+            base64_authorization_header is valid.
+            None: If base64_authorization_header is None,
+            not a string, or not valid Base64.
+        """
+        if base64_authorization_header is None or \
+                not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            base64_bytes = base64_authorization_header.encode('utf-8')
+            decoded_bytes = base64.b64decode(base64_bytes)
+            decoded_str = decoded_bytes.decode('utf-8')
+            return decoded_str
+        except Exception:
+            return None
+
     def extract_base64_authorization_header(
             self, authorization_header: str) -> str:
         """Extracts the Base64 encoded part of the Authorization header.
