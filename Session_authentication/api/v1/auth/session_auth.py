8fb034ea36c89cb03f48979f899357ec05d8a1e9
NikkiMerena <alderman.nikki1022@gmail.com>
2023-09-26 20:47:56 -0500

    task3

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -25,3 +25,16 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())  # Generate a Session ID using uuid4()
         self.user_id_by_session_id[session_id] = user_id
         return session_id  # Return the generated Session ID
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Returns a User ID based on a Session ID
+
+        :param session_id: the session id for which to retrieve the user id
+        :type session_id: str
+        :return: the User ID associated with the session id
+        :rtype: str
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
