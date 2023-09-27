b5dd8bf79660e141ad3f2de1e7193cae370b77b1
NikkiMerena <alderman.nikki1022@gmail.com>
2023-09-26 22:56:56 -0500

    tas8

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python3
-""" Model: """
+"""
+This module defines the SessionAuth class, which extends the Auth class
+to manage user sessions for authentication. 
+
+The SessionAuth class allows for the creation of new user sessions, 
+retrieving User IDs based on Session IDs, and destroying sessions 
+to log out users. This is achieved through the use of a dictionary 
+mapping session IDs to user IDs, and the UUID library for generating 
+unique session IDs.
+
+Classes:
+    SessionAuth: Inherits from Auth and manages user sessions.
+"""
+
 
 from api.v1.auth.auth import Auth
 import uuid
@@ -38,3 +51,27 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def destroy_session(self, request=None):
+        """
+        Deletes the user session / logout.
+
+        :param request: The request object.
+        :type request: Request
+        :return: True if the session was successfully destroyed, else False
+        :rtype: bool
+        """
+        if request is None:
+            return False
+
+
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return False
+
+        del self.user_id_by_session_id[session_id]
+        return True
