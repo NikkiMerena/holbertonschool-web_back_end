2244a591270b1a2ed39c210f3ad023dc728f7e32
NikkiMerena <alderman.nikki1022@gmail.com>
2023-09-27 21:02:54 -0500

    task5

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -2,14 +2,32 @@
 """_summary_
 """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
-def _hash_password(password: str) -> bytes:
-    """
-    Takes in a password string argument and returns bytes.
-    The returned bytes is a salted hash of the input password,
-    hashed with bcrypt.hashpw.
-    """
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self):
+        self._db = DB()
+
+    def _hash_password(self, password: str) -> bytes:
+        """Hash a password and return the salted hash."""
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+        return hashed_password
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a user."""
+        try:
+            # Try to find a user by email
+            user = self._db.find_user_by(email=email)
+            # If the user is found, raise a ValueError
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            # If NoResultFound is raised, hash the password
+            hashed_password = self._hash_password(password)
+            # Add user to the database and return the User object
+            return self._db.add_user(email, hashed_password)
