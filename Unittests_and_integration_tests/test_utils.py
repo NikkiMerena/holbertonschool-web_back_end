b2cc56fb832be41d876a518a5b743c219f49d148
NikkiMerena <alderman.nikki1022@gmail.com>
2023-10-03 13:09:35 -0500

    task1

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -16,3 +16,18 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """Test method for access_nested_map"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Class for testing access_nested_map function"""
+    
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test access_nested_map raises a KeyError"""
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, path)
+        
+        # If you need to check the exception message, you can use:
+        # self.assertEqual(str(e.exception), "Your expected exception message")
