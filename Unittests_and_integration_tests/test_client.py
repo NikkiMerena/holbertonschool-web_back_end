9d6672c64f20c73b87e08401bbc8a24db3d11bca
NikkiMerena <alderman.nikki1022@gmail.com>
2023-10-04 09:55:44 -0500

    task6

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -45,3 +45,33 @@ class TestGithubOrgClient(unittest.TestCase):
             client._public_repos_url,
             "https://api.github.com/orgs/example/repos"
         )
+
+    @patch('client.get_json', return_value=[
+        {'name': 'repo1'},
+        {'name': 'repo2'}
+    ])
+    def test_public_repos(self, mock_get_json):
+        """
+        Test that the list of repos is what you expect from the chosen payload.
+        """
+        with patch(
+            'client.GithubOrgClient._public_repos_url',
+            new_callable=PropertyMock
+        ) as mock_public_repos_url:
+            # Define a custom return value for _public_repos_url property
+            mock_public_repos_url.return_value = (
+                "https://api.github.com/orgs/example/repos"
+            )
+
+            # Initialize an object of GithubOrgClient
+            client = GithubOrgClient('example')
+
+            # Call the public_repos method, and get the repo names
+            repos = client.public_repos()
+
+            # Assertions to validate the outcomes
+            self.assertEqual(repos, ['repo1', 'repo2'])
+            mock_get_json.assert_called_once_with(
+                "https://api.github.com/orgs/example/repos"
+            )
+            mock_public_repos_url.assert_called_once_with()
