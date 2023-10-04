faefdbc06629f12afd16c81223e623c6887a688b
NikkiMerena <alderman.nikki1022@gmail.com>
2023-10-04 08:55:55 -0500

    task5

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -2,8 +2,9 @@
 """Test module for client.GithubOrgClient"""
 import unittest
 from parameterized import parameterized
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 from client import GithubOrgClient
+
 
 class TestGithubOrgClient(unittest.TestCase):
     """Class for testing GithubOrgClient"""
@@ -23,4 +24,24 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(test_class_instance.org, {"payload": True})
         mock_get_json.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}"
+        )
+
+    @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        """Test that the result of _public_repos_url is the
+        expected one based on the mocked payload."""
+
+        # Creating a payload that org is expected to return
+        payload = {"repos_url": "https://api.github.com/orgs/example/repos"}
+
+        # Setting the return_value of the mocked property
+        mock_org.return_value = payload
+
+        # Creating an instance of GithubOrgClient
+        client = GithubOrgClient("example")
+
+        # Testing _public_repos_url returns the correct value
+        self.assertEqual(
+            client._public_repos_url,
+            "https://api.github.com/orgs/example/repos"
         )
