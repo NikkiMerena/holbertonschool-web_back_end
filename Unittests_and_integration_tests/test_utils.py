d4947dca0d20819844854ebc33d9d5e6c17ce803
NikkiMerena <alderman.nikki1022@gmail.com>
2023-10-03 22:07:17 -0500

    fixing error

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -61,20 +61,21 @@ class TestMemoize(unittest.TestCase):
         """Test that when calling a_property twice,
         the correct result is returned but a_method is only called once."""
 
-    class TestClass:
-        """Sample class for testing memoization"""
+        class TestClass:
+            """Sample class for testing memoization"""
 
-        def a_method(self):
-            """Method to be mocked and called once"""
-            return 42
+            def a_method(self):
+                """Method to be mocked and called once"""
+                return 42
 
-    @memoize
-    def a_property(self):
-        """Property method that uses memoization"""
-        return self.a_method()
+            @memoize
+            def a_property(self):
+                """Property method that uses memoization"""
+                return self.a_method()
 
-    with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
-        test_obj = TestClass()
-        self.assertEqual(test_obj.a_property, 42)
-        self.assertEqual(test_obj.a_property, 42)
-        mock_method.assert_called_once()
+        with patch.object(TestClass, 'a_method', return_value=42) \
+                as mock_method:
+            test_obj = TestClass()
+            self.assertEqual(test_obj.a_property, 42)
+            self.assertEqual(test_obj.a_property, 42)
+            mock_method.assert_called_once()
