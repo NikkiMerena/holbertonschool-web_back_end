1864c2a6930ba2295d695b16ea7cef3e1a89039f
NikkiMerena <alderman.nikki1022@gmail.com>
2023-10-04 10:22:34 -0500

    task7

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -75,3 +75,18 @@ class TestGithubOrgClient(unittest.TestCase):
                 "https://api.github.com/orgs/example/repos"
             )
             mock_public_repos_url.assert_called_once_with()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected_output):
+        """Test GithubOrgClient.has_license method"""
+
+        client = GithubOrgClient("example")
+
+        # Testing if has_license returns the expected output
+        self.assertEqual(
+            client.has_license(repo, license_key),
+            expected_output
+        )
