e637c76a4845be55587d6947c9fa14b6471cf830
NikkiMerena <alderman.nikki1022@gmail.com>
2023-10-04 19:39:30 -0500

    trying again

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-"""summary"""
-from flask import Flask, render_template
+"""Simple flask app with index.html template"""
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
@@ -9,14 +9,27 @@ babel = Babel(app)
 
 
 class Config:
+    """Class which configs available lang"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app.config.from_object(Config)
+babel = Babel(app)
 
 
 @app.route('/')
-def home():
+def index():
+    """route for '/'"""
     return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """Retrieves locale from request"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run()
