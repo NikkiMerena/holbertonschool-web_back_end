58235fa2a1606b81d818bb39da62250b02f6d3cc
NikkiMerena <alderman.nikki1022@gmail.com>
2023-10-19 02:04:21 +0000

    task2 fixed error

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,15 +1,6 @@
--- Create a temporary table to store the results
-CREATE TEMPORARY TABLE IF NOT EXISTS temp_results AS (
-    SELECT origin, SUM(nb_fans) AS nb_fans
-    FROM metal_bands
-    GROUP BY origin
-);
-
--- Rank the country origins by the number of (non-unique) fans
-SET @rank = 0;
-SELECT origin, nb_fans, (@rank := @rank + 1) AS rank
-FROM temp_results
+-- Task 2 Ranks Country origins of bands,
+-- ordered by the number of (non-unique) fans
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
 ORDER BY nb_fans DESC;
-
--- Drop the temporary table
-DROP TEMPORARY TABLE IF EXISTS temp_results;
