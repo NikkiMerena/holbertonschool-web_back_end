a174818951eb258777709b85fa42a70b2ea32750
NikkiMerena <alderman.nikki1022@gmail.com>
2023-10-29 18:32:51 +0000

    adding intial files and task 0

---
+++ Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+// 0-console.js
+
+// Define the displayMessage function that takes a messafe as an argument.
+function displayMessage(message) {
+    // Print the message to the standard output (STDOUT).
+    console.log(message);
+}
+
+// Export the displayMessage function so it can be used in other files.
+module.exports = displayMessage;
