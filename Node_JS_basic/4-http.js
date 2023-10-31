1fb4841f548fbcee6cae331e4291112a58bb0e44
NikkiMerena <alderman.nikki1022@gmail.com>
2023-10-31 01:18:20 +0000

    task4

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,17 +1,11 @@
 const http = require('http');
 
-// Create an HTTP server.
 const app = http.createServer((req, res) => {
-  // Set the response status and headers.
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-
-  // Send the response body.
-  res.end('Hello Holberton School!\n');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
-// Listen on port 1245.
-const port = 1245;
-app.listen(port);
+app.listen(1245);
 
-// Export the app variable.
 module.exports = app;
