8a39e53468e73518fc5a8da16a0ff747139473d4
NikkiMerena <alderman.nikki1022@gmail.com>
2023-10-31 01:26:43 +0000

    task3

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -2,48 +2,47 @@ const fs = require('fs');
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    // Attempt to read the database file asynchronously.
-    fs.readFile(path, 'utf8', (error, data) => {
-      if (error) {
-        // If there's an error reading the file, reject the Promise.
-        reject(new Error('Cannot load the database'));
-      } else {
-        // Split the data into lines.
-        const lines = data.split('\n');
-
-        // Initialize variables to count students in each field.
-        let totalStudents = 0;
-        let csStudents = 0;
-        let sweStudents = 0;
-        const csList = [];
-        const sweList = [];
-
-        // Loop through each line and process student data.
-        for (const line of lines) {
-          const [firstname, lastname, age, field] = line.split(',');
-
-          // Check if the line contains valid student data.
-          if (firstname && lastname && age && field) {
-            totalStudents += 1;
-
-            if (field === 'CS') {
-              csStudents += 1;
-              csList.push(firstname);
-            } else if (field === 'SWE') {
-              sweStudents += 1;
-              sweList.push(firstname);
-            }
-          }
-        }
-
-        // Log the results.
-        console.log(`Number of students: ${totalStudents}`);
-        console.log(`Number of students in CS: ${csStudents}. List: ${csList.join(', ')}`);
-        console.log(`Number of students in SWE: ${sweStudents}. List: ${sweList.join(', ')}`);
-
-        // Resolve the Promise.
-        resolve();
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+        return;
       }
+      const response = [];
+      let msg;
+
+      const content = data.toString().split('\n');
+
+      let students = content.filter((item) => item);
+
+      students = students.map((item) => item.split(','));
+
+      const NUMBER_OF_STUDENTS = students.length ? students.length - 1 : 0;
+      msg = `Number of students: ${NUMBER_OF_STUDENTS}`;
+      console.log(msg);
+
+      response.push(msg);
+
+      const fields = {};
+      for (const i in students) {
+        if (i !== 0) {
+          if (!fields[students[i][3]]) fields[students[i][3]] = [];
+
+          fields[students[i][3]].push(students[i][0]);
+        }
+      }
+
+      delete fields.field;
+
+      for (const key of Object.keys(fields)) {
+        msg = `Number of students in ${key}: ${
+          fields[key].length
+        }. List: ${fields[key].join(', ')}`;
+
+        console.log(msg);
+
+        response.push(msg);
+      }
+      resolve(response);
     });
   });
 }
